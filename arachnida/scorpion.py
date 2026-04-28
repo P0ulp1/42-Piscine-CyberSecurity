21307c2bbd01f83571c0759d982bd65a25f15dd5
Paul Hautenauve <phautena@f1br9s8.clusters.42paris.fr>
2026-04-28 15:32:26 +0200

    ./scorpion.py --> Resolved error due to printing int + str (need to cast often in python)
    ./spider.py
    --> Exif data is now displaying

--- arachnida/scorpion.py
+++ arachnida/scorpion.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from exif import Image
+from PIL import Image
 
 parser = argparse.ArgumentParser(prog="./scorpion.py", description="Retrieve EXIF and other metadata of provided image files. Supported file types are: .jpg/jpeg, .png, .bmp, .gif", epilog="Made by @P0ulp1")
 parser.add_argument('FILE1 [FILE2 ...]', nargs='+')
@@ -15,17 +15,17 @@ def placeholder():
 def reading_metadata():
     try:
         for img in all_images:
-            with open(img, 'rb') as img_file:
-                meta = Image(img_file)
-                if (meta.has_exif == True):
-                    print("IT HAS METADATA")
-                else:
-                    print("NOT METADATA FOUND")
-                print(dir(meta))
-                print(meta.get_all())
+            im = Image.open(img)
+            exif = im.getexif()
+            print(f"EXIF data for {img}:")
+            for tag_id, value in exif.items():
+                tag_name = Image.ExifTags.TAGS.get(tag_id, tag_id)
+                print(f"{tag_name}: {value}")
+            print("\n")
     except BaseException as e:
         print("An unexpected error occured: ", e)
 
 if (__name__ == "__main__"):
-    placeholder()
+    # placeholder()
     reading_metadata()
+    
--- arachnida/spider.py
+++ arachnida/spider.py
@@ -106,7 +106,7 @@ def main():
 	handle_download_path()
 	get_all_links(0, recursive_depth, args["URL"])
 	download_images()
-	print("Successfully downloaded ", len(images_links) + "images")
+	print("Successfully downloaded ", str(len(images_links)) + " images")
 
 if (__name__ == "__main__"):
 	main()
